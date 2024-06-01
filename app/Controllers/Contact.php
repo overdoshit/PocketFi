f333f88c8b4b98528647621e2fdd0a1a1621035c
Faiz Kurniawan <mbfzak1@gmail.com>
2024-06-01 23:04:14 +0700

    Fix typo

--- app/Controllers/Contact.php
+++ app/Controllers/Contact.php
@@ -4,8 +4,7 @@ namespace App\Controllers;
 
 class Contact extends BaseController
 {
-    public function index()
-    {
+    public function index() {
         $data = [
             'title' => 'Contact | Pocket Fi'
         ];
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,7 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index()
-    {
+    public function index() {
         $data = [
             'title' => 'Home | Pocket Fi'
         ];
