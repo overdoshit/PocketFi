7f56c4db4047cb8084181174658e5118d6acc5b0
Faiz Kurniawan <mbfzak1@gmail.com>
2024-06-08 00:00:37 +0700

    Fix footer menu

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,15 +1,7 @@
 <div class="container mt-auto">
     <footer class="py-2">
         <div class="row">
-            <div class="col-md-5 mb-3">
-                <!-- <h3>Pocket Fi</h3> -->
-                <img src="/assets/images/logo.png" alt="Bootstrap" height="55">
-                <p>The Pocket Wifi is fully activated and ready to go so as soon as you land abroad, turn it on and it works! Connect the whole travel group to your Mobile WiFi or enjoy your mobile broadband with multiple gadgets.</p>
-                <h6>Address</h6>
-                <p>Jl. Letnan Sutopo BSD Serpong Lengkong Gudang Timur, Rw. Mekar Jaya, Kec. Serpong, Kota Tangerang Selatan</p>
-            </div>
-
-            <div class="col-6 col-md-2 mb-3 offset-md-1 mt-3">
+            <div class="col-6 col-md-2 mb-3 mt-2">
                 <h5>Pages</h5>
                 <ul class="nav flex-column">
                     <li class="nav-item mb-2"><a href="/" class="nav-link p-0 text-body-secondary">Home</a></li>
@@ -19,13 +11,23 @@
                 </ul>
             </div>
 
-            <div class="col-6 col-md-2 mb-3 offset-md-1 mt-3">
+            <div class="col-6 col-md-2 mb-3 mt-2">
                 <h5>Support</h5>
                 <ul class="nav flex-column">
                     <li class="nav-item mb-2"><a href="/how-it-works" class="nav-link p-0 text-body-secondary">How it Works</a></li>
                     <li class="nav-item mb-2"><a href="/faq" class="nav-link p-0 text-body-secondary">FAQ</a></li>
                     <li class="nav-item mb-2"><a href="/contact" class="nav-link p-0 text-body-secondary">Contact Us</a></li>
                 </ul>
+            </div>
+
+            <div class="col-6 col-md-3 mb-3 mt-2">
+                <h5>Address</h5>
+                <p class="text-body-secondary">Jl. Letnan Sutopo BSD Serpong Lengkong Gudang Timur, Rw. Mekar Jaya, Kec. Serpong, Kota Tangerang Selatan</p>
+            </div>
+
+            <div class="col-md-5 mb-3">
+                <img src="/assets/images/logo.png" alt="Pocket Fi" height="30" style="margin: 5px 0;">
+                <p class="text-body-secondary">The Pocket Wifi is fully activated and ready to go so as soon as you land abroad, turn it on and it works! Connect the whole travel group to your Mobile WiFi or enjoy your mobile broadband with multiple gadgets.</p>
             </div>
         </div>
 
