83c7863fa2a7926e3b31c326714680b896121ff1
Faiz Kurniawan <mbfzak1@gmail.com>
2024-06-10 22:00:51 +0700

    Update mifi indonesia

--- app/Views/Users/mobile_wifi_indonesia.php
+++ app/Views/Users/mobile_wifi_indonesia.php
@@ -93,11 +93,18 @@
     }
 
     .best-seller {
-        background: linear-gradient(#D90809 0%, #AE0607 100%);
+        background: linear-gradient(#2E8A5C 0%, #256e4a 100%);
+        /* 20% Darken */
     }
 
     .low-stock {
         background: linear-gradient(#F39C12 0%, #DB8C10 100%);
+        /* 20% Darken */
+    }
+
+    .sold-out {
+        background: linear-gradient(#D90809 0%, #AE0607 100%);
+        /* 20% Darken */
     }
 
     .ribbon span::before {
@@ -122,22 +129,25 @@
     <div class="row row-cols-1 row-cols-md-2 mb-3 text-center">
         <?php foreach ($products as $product) : ?>
             <div class="product col position-relative">
-                <?php if ($product['bestSeller'] === 'Y') : ?>
+                <?php if ($product['stock'] == 0) : ?>
+                    <div class="ribbon"><span class="sold-out">Sold Out</span></div>
+                <?php elseif ($product['stock'] <= 2) : ?>
+                    <div class="ribbon"><span class="low-stock">Low Stock</span></div>
+                <?php elseif ($product['bestSeller'] === 'Y') : ?>
                     <div class="ribbon"><span class="best-seller">Best Seller</span></div>
                 <?php endif; ?>
-                <?php if ($product['stock'] <= 3) : ?>
-                    <div class="ribbon"><span class="low-stock">Low Stock</span></div>
-                <?php endif; ?>
-                <div class="card mb-5 rounded-3 shadow-sm border-primary">
-                    <div class="card-header py-3 text-bg-primary border-primary" style="min-height: 72px;">
-                        <h1 class="text-uppercase fw-bold text-start fs-6 m-0"><?= $product['country']; ?> <?= $product['packageType']; ?> <?= $product['dataUsage']; ?> IDR <?= number_format($product['price'], 0, ',', '.'); ?>/<?= $product['priceType']; ?> (MINIMUM RENT <?= $product['minimumRentDays']; ?> DAYS)</h1>
+                <div class="card mb-5 rounded-3 shadow-sm <?= $product['stock'] == 0 ? 'border-secondary' : 'border-primary'; ?>">
+                    <div class="card-header py-3 <?= $product['stock'] == 0 ? 'text-bg-secondary border-secondary' : 'text-bg-primary border-primary'; ?>" style="min-height: 72px;">
+                        <h1 class="text-uppercase fw-bold text-start fs-6 m-0">
+                            <?= $productName = $product['country'] . ' ' . $product['packageType'] . ' ' . $product['dataUsage'] . ' IDR ' . number_format($product['dailyPrice'], 0, ',', '.') . '/DAY (MINIMUM RENT ' . $product['minimumRentDays'] . ' DAYS)'; ?>
+                        </h1>
                     </div>
                     <div class="card-body">
                         <table class="table table-products">
                             <tbody>
                                 <tr>
                                     <td class="dataProduct" rowspan="2" style="vertical-align : middle;">
-                                        <img src="<?= $product['countryFlagUrl']; ?>" width="115px" style="box-shadow: 0px 4px 8px 0px #0000004D;">
+                                        <img src="<?= $product['countryFlagUrl']; ?>" width="115px" style="box-shadow: 0px 4px 8px 0px #0000004D; <?= $product['stock'] == 0 ? 'filter: grayscale(100%);' : ''; ?>">
                                     </td>
                                     <td class="dataProduct">
                                         <div class="title-row">Country</div>
@@ -162,16 +172,36 @@
                                         <p class="content-row fw-bold">IDR <?= number_format($product['deposit'], 0, ',', '.'); ?></p>
                                     </td>
                                     <td class="dataProduct">
-                                        <button type="button" class="btn btn-warning btn-more m-2" data-bs-toggle="modal" data-bs-target="#detailModal-<?= $product['idProduct']; ?>">More details &raquo;</button>
+                                        <button type="button" class="btn <?= $product['stock'] == 0 ? 'btn-secondary' : 'btn-warning'; ?> btn-more m-2" data-bs-toggle="modal" data-bs-target="#detailModal-<?= $product['idProduct']; ?>">More details &raquo;</button>
                                     </td>
                                 </tr>
                             </tbody>
                         </table>
-                        <h5 class="fw-bold my-2">IDR <?= number_format($product['price'], 0, ',', '.'); ?>/ <span class="text-lowercase"><?= $product['priceType']; ?></span></h5>
+                        <h5 class="price-label fw-bold my-2">
+                            IDR <?= number_format($product['packageType'] == 'WEEKLY' ? $product['dailyPrice'] * 7 : ($product['packageType'] == 'MONTHLY' ? $product['dailyPrice'] * 30 : $product['dailyPrice']), 0, ',', '.'); ?>/
+                            <span class="text-lowercase">
+                                <?php
+                                switch ($product['packageType']) {
+                                    case 'DAILY':
+                                        echo 'day';
+                                        break;
+                                    case 'WEEKLY':
+                                        echo '7 days';
+                                        break;
+                                    case 'MONTHLY':
+                                        echo '30 days';
+                                        break;
+                                    default:
+                                        echo 'day';
+                                        break;
+                                }
+                                ?>
+                            </span>
+                        </h5>
                         <?php if (session('email')) : ?>
-                            <button type="button" class="w-100 btn btn-lg btn-primary" data-bs-toggle="modal" data-bs-target="#bookingModal-<?= $product['idProduct']; ?>">Check out</button>
+                            <button type="button" class="w-100 btn btn-lg <?= $product['stock'] == 0 ? 'btn-secondary' : 'btn-primary'; ?>" <?= $product['stock'] == 0 ? 'disabled' : ''; ?> data-bs-toggle="modal" data-bs-target="#bookingModal-<?= $product['idProduct']; ?>">Check out</button>
                         <?php else : ?>
-                            <button type="button" class="w-100 btn btn-lg btn-primary" data-bs-toggle="modal" data-bs-target="#loginModal">Check out</button>
+                            <button type="button" class="w-100 btn btn-lg <?= $product['stock'] == 0 ? 'btn-secondary' : 'btn-primary'; ?>" <?= $product['stock'] == 0 ? 'disabled' : ''; ?> data-bs-toggle="modal" data-bs-target="#loginModal">Check out</button>
                         <?php endif; ?>
                     </div>
                 </div>
@@ -181,8 +211,10 @@
                 <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered">
                     <div class="modal-content rounded-4 shadow">
                         <div class="modal-header p-4 mx-2 border-bottom-1">
-                            <h1 class="text-uppercase fw-bold mb-0 fs-5" id="detailModalLabel-<?= $product['idProduct']; ?>"><?= $product['country']; ?> <?= $product['packageType']; ?> <?= $product['dataUsage']; ?> IDR <?= number_format($product['price'], 0, ',', '.'); ?>/<?= $product['priceType']; ?> (MINIMUM RENT <?= $product['minimumRentDays']; ?> DAYS)</h1>
-                            <button type="button" class="btn-close ms-1" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <h1 class="text-uppercase fw-bold text-start fs-6 m-0">
+                                <?= $productName; ?>
+                            </h1>
+                            <button type="button" class="btn-close text-end" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
                             <div class="row">
@@ -245,9 +277,9 @@
                                     <div><?= $product['notes']; ?></div>
                                 </div>
                             </div>
-                            <div class="modal-footer p-0">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" aria-label="Close">Close</button>
-                            </div>
+                        </div>
+                        <div class="modal-footer px-4">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" aria-label="Close">Close</button>
                         </div>
                     </div>
                 </div>
@@ -257,21 +289,24 @@
                 <div class="modal-dialog modal-dialog-centered">
                     <div class="modal-content rounded-4 shadow">
                         <div class="modal-header p-4 mx-2 border-bottom-1">
-                            <h1 class="text-uppercase fw-bold mb-0 fs-5" id="bookingModalLabel-<?= $product['idProduct']; ?>"><?= $product['country']; ?> <?= $product['packageType']; ?> <?= $product['dataUsage']; ?> IDR <?= number_format($product['price'], 0, ',', '.'); ?>/<?= $product['priceType']; ?> (MINIMUM RENT <?= $product['minimumRentDays']; ?> DAYS)</h1>
+                            <h1 class="text-uppercase fw-bold text-start fs-6 m-0">
+                                <?= $productName; ?>
+                            </h1>
                             <button type="button" class="btn-close ms-1" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <form action="/booking" method="post">
                             <div class="modal-body">
                                 <h5 class="text-center mb-4">Set how long you want to borrow wifi</h5>
-                                <input type="hidden" name="product" value="<?= $product['idProduct']; ?>">
-                                <input type="hidden" name="per" value="<?= $product['packageType']; ?>">
+                                <input type="hidden" name="idProduct" value="<?= $product['idProduct']; ?>">
+                                <input type="hidden" name="productName" value="<?= $productName; ?>">
                                 <div class="date-book">
                                     <div class="form-floating mb-4">
-                                        <input type="date" class="form-control rounded-3 start-date" id="startDate" name="startDate" min="<?= date("Y-m-d"); ?>" required>
+                                        <input type="date" class="form-control rounded-3 start-date" id="startDate" name="startDate" min="<?= date('Y-m-d'); ?>" value="<?= old('startDate') ?>" required>
                                         <label for="floatingInput">Start Date</label>
                                     </div>
                                     <div class="form-floating mb-4">
-                                        <input type="date" class="form-control rounded-3 start-date" id="endDate" name="endDate" required>
+                                        <?php $minEndDate = date('Y-m-d', strtotime('+' . ($product['minimumRentDays']) . ' days')); ?>
+                                        <input type="date" class="form-control rounded-3 end-date" id="endDate" name="endDate" min="<?= $minEndDate; ?>" required>
                                         <label for="floatingInput">End Date</label>
                                     </div>
                                 </div>
@@ -288,7 +323,5 @@
         <?php endforeach; ?>
     </div>
 </section>
-
-
 
 <?= $this->include('Templates/footer'); ?>
