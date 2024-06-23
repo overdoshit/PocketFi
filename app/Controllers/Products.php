a086180e91aa94275a702787b5d2a1d189cdc1d2
Faiz Kurniawan <mbfzak1@gmail.com>
2024-06-24 04:29:04 +0700

    Update dynamic list products

--- app/Controllers/Products.php
+++ app/Controllers/Products.php
@@ -3,14 +3,23 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\ProductsModel;
 
 class Products extends BaseController
 {
+    protected $products;
+
+    public function __construct()
+    {
+        $this->products = new ProductsModel();
+    }
+
     public function index()
     {
         $data = [
             'title' => 'Admin Products · Pocket Fi',
             'pageTitle' => 'Products',
+            'products' => $this->products->findAll(),
         ];
 
         return view('Admins/products.php', $data);
